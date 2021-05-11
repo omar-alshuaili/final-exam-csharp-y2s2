d5995d82707f292d4081ca84480cc852c4ed38f9
Omar Alshuaili <S00190262@mail.itsligo.ie>
2021-05-11 10:03:49 +0100

    adding properties to GameClass

--- OmarAlshuaili_s00190262/Gmae.cs
+++ OmarAlshuaili_s00190262/Gmae.cs
@@ -8,5 +8,31 @@ namespace OmarAlshuaili_s00190262
 {
     public class Game
     {
+        public string Name { get; set; }
+        public int MetacriticScore { get; set; }
+        public string Game_Image { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+
+      
+
+        public Game(string Name,string Platform, int CriticScore, decimal Price ,string Description, string GameImage = "")
+        {
+            Name = this.Name;
+            Description = this.Description;
+            CriticScore = this.MetacriticScore;
+            Platform = this.Platform;
+            Price = this.Price;
+            GameImage = this.Game_Image;
+
+
+
+
+
+        }
+
+
+
     }
 }
