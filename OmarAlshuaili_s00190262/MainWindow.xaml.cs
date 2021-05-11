94d8221b087e904265c7ee3b635e94078a92e1d5
Omar Alshuaili <S00190262@mail.itsligo.ie>
2021-05-11 12:41:18 +0100

    final commit

--- OmarAlshuaili_s00190262/MainWindow.xaml.cs
+++ OmarAlshuaili_s00190262/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace OmarAlshuaili_s00190262
     public partial class MainWindow : Window
     {
         GameData db = new GameData();
+        List<Game> Filtergame = new List<Game>();
         public MainWindow()
         {
             InitializeComponent();
@@ -28,13 +29,81 @@ namespace OmarAlshuaili_s00190262
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //display all games
             var q = from g in db.games
-                    select new
-                    {
-                        Score = g.MetacriticScore,
-                        image = g.Game_Image
-                    };
+                    select g;
             gameListBox.ItemsSource = q.ToList();
+        }
+
+        private void gameListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //get the selected item
+            var selectedItem = gameListBox.SelectedItem as Game;
+            //check if null
+            if (selectedItem != null)
+            {
+                priceTBL.Text = selectedItem.Price.ToString();
+                DesTBL.Text = selectedItem.Description.ToString();
+                PlatTBL.Text = selectedItem.Platform.ToString();
+              
+            }
+        
+        }
+
+        private void allRB_Checked(object sender, RoutedEventArgs e)
+        {
+            //get all games
+            var q = from g in db.games
+                    select g;
+            gameListBox.ItemsSource = q.ToList();
+
+        }
+
+        private void PsRB_Checked(object sender, RoutedEventArgs e)
+        {
+            //update list box
+            update("Ps");
+
+        }
+
+        private void XboxRB_Checked(object sender, RoutedEventArgs e)
+        {
+            //update list box
+            update("Xbox");
+
+            
+
+        }
+
+        private void PcRB_Checked(object sender, RoutedEventArgs e)
+        {
+            //update list box
+            update("Pc");
+        }
+
+        private void SwitchRB_Checked(object sender, RoutedEventArgs e)
+        {
+            //update list box
+            update("Switch");
+           
+        }
+        public void update(string platform)
+        {
+            gameListBox.ItemsSource = null;
+            //clear the filtered list
+            Filtergame.Clear();
+
+            var q = from Pc in db.games
+                    where Pc.Platform.Contains(platform)
+                    select Pc;
+            var result = q.ToList();
+
+            foreach (var item in result)
+            {
+                Filtergame.Add(item);
+            }
+            //display the filtered list
+            gameListBox.ItemsSource = Filtergame;
         }
     }
 }
